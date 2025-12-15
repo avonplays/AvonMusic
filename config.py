c367f8a01b0ddf089d38d285598fa06bfee4de25
avonplays <avonplays21@gmail.com>
2025-12-15 12:59:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 999))
 LOG_GROUP_ID = -1002353470224
 
 # Get this value from @ultron2_robot on Telegram by /id
-OWNER_ID = 6887657313
+OWNER_ID = 7895661089
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -112,5 +112,6 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
